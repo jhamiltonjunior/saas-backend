1f813f04d9acda8a8b9a09330cf7018a47f152ea
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-06-24 20:01:39 -0300

    feature: add make update article in routes/artilce

--- src/main/routes/article.ts
+++ src/main/routes/article.ts
@@ -5,12 +5,13 @@ import { makeCreateArticleController } from '../factories/articles/create'
 import { makeShowUniqueArticleConstroller } from '../factories/articles/showUnique'
 import { makeShowAllArticleController } from '../factories/articles/showAll'
 import { makeDeleteArticleController } from '../factories/articles/delete'
+import { makeUpdateArticleController } from '../factories/articles/update'
 
 export default (router: Router): void => {
   router.get('/article', adapterRouteShowArticle(makeShowAllArticleController()))
   router.get('/article/:url', adapterRouteShowArticle(makeShowUniqueArticleConstroller()))
   router.post('/article', adapterRouteWithAuthentication(makeCreateArticleController()))
-  router.post('/article/:url', adapterRouteWithAuthentication(makeCreateArticleController()))
+  router.post('/article/:url', adapterRouteWithAuthentication(makeUpdateArticleController()))
   router.delete('/article/:url', adapterRouteWithAuthentication(makeDeleteArticleController()))
 
   router.get('/article/test', (req: Request, res: Response) => {
