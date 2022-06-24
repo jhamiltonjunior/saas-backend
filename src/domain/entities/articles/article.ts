45c87cf6823a14893b3b78c1bdaf9829f911a5a7
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-06-24 17:57:11 -0300

    refactor: method update added in domain/entities/article.ts

--- src/domain/entities/articles/article.ts
+++ src/domain/entities/articles/article.ts
@@ -62,6 +62,59 @@ export class Article {
     const createdAtOrError: Either<InvalidCreatedAtError, CreatedAt> = CreatedAt.create(article.createdAt)
     const titleOrError: Either<InvalidTitleError, Title> = Title.create(article.title)
     const urlOrError: Either<InvalidURLError, URL> = URL.create(article.url)
+    // const commentaryOrError: Either<InvalidCommentaryError, Commentary> = Commentary.create(article.commentary)
+
+    if (authorOrError.isLeft()) {
+      return left(authorOrError.value)
+    }
+    if (bodyOrError.isLeft()) {
+      return left(bodyOrError.value)
+    }
+    if (categoryOrError.isLeft()) {
+      return left(categoryOrError.value)
+    }
+    if (createdAtOrError.isLeft()) {
+      return left(createdAtOrError.value)
+    }
+    if (titleOrError.isLeft()) {
+      return left(titleOrError.value)
+    }
+    if (urlOrError.isLeft()) {
+      return left(urlOrError.value)
+    }
+
+    const title: Title = titleOrError.value
+    const body: Body = bodyOrError.value
+    const author: Author = authorOrError.value
+    const category: Category = categoryOrError.value
+    const createdAt: CreatedAt = createdAtOrError.value
+    const url: URL = urlOrError.value
+
+    return right(new Article(
+      title,
+      body,
+      author,
+      category,
+      createdAt,
+      url,
+    ))
+  }
+
+  static update (article: IArticleData): Either<
+  InvalidTitleError |
+  InvalidBodyError |
+  InvalidAuthorError |
+  InvalidCategoryError |
+  InvalidCreatedAtError |
+  InvalidURLError |
+  InvalidUpdatedAtError,
+  Article> {
+    const authorOrError: Either<InvalidAuthorError, Author> = Author.create(article.author)
+    const bodyOrError: Either<InvalidBodyError, Body> = Body.create(article.body)
+    const categoryOrError: Either<InvalidCategoryError, Category> = Category.create(article.category)
+    const createdAtOrError: Either<InvalidCreatedAtError, CreatedAt> = CreatedAt.create(article.createdAt)
+    const titleOrError: Either<InvalidTitleError, Title> = Title.create(article.title)
+    const urlOrError: Either<InvalidURLError, URL> = URL.create(article.url)
     const updatedAtOrError: Either<InvalidUpdatedAtError, UpdatedAt> = UpdatedAt.create(article.updatedAt)
     // const commentaryOrError: Either<InvalidCommentaryError, Commentary> = Commentary.create(article.commentary)
 
@@ -102,7 +155,7 @@ export class Article {
       category,
       createdAt,
       url,
-      updatedAt || undefined,
+      updatedAt,
     ))
   }
 }
