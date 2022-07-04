a1100cf5ef6bfd326ffae195dd7359c0f5f38846
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-07-03 21:54:47 -0300

    cors imported in config/app and executed

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import routes from './routes'
 import middlewares from './middlewares'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors())
 
 middlewares(app)
 routes(app)
