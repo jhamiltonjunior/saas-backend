f9123245dab81d9d112ca7517a9413a11b143ad3
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-07-07 19:14:19 -0300

    test: Should not create title with Much characters added

---
+++ test/domain/entities/articles/validators/title.spec.ts
@@ -0,0 +1,24 @@
+import { InvalidTitleError } from "../../../../../src/domain/entities/articles/errors/invalidTitle"
+import { Title } from "../../../../../src/domain/entities/articles/validators/title"
+
+describe('Title Domain Validator', () => {
+  it('Should not create title with little characters', () => {
+    const content = '123456789'
+
+    const title = Title.create(content)
+
+    expect(title).toEqual({value: new InvalidTitleError(content)})
+  })
+
+  it('Should not create title with Much characters', () => {
+    let content = ''
+
+    for (let i = 256; content.length < i; i) {
+      content += 'o'
+    }
+
+    const title = Title.create(content)
+
+    expect(title).toEqual({value: new InvalidTitleError(content)})
+  })
+})
