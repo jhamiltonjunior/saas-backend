318240c961fa8fdf778e87e4ee42951f620c70ee
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-07-10 15:00:51 -0300

    refactor: dist deleted

--- dist/src/main/config/middlewares.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const middleware_1 = require("../middleware");
-exports.default = (app) => {
-    app.use(middleware_1.bodyParser);
-    app.use(middleware_1.contentType);
-};
