4b9ec511505ccd6a621ed923bee72aa666dd9b4c
Kessia Santos <kessiasantts11@gmail.com>
2023-05-07 21:07:28 -0300

    before and after tests logic added, but not tested

--- test/main/server.test.ts
+++ test/main/server.test.ts
@@ -16,9 +16,17 @@ middlewares(app)
 routes(app)
 
 describe('Testing', () => {
+	let server
   before(async () => {
     // _server = (await import('../../src/main/config/app')).default
-    await new Promise(resolve => app.once('listening', resolve))
+    // await new Promise(resolve => app.once('listening', resolve))
+	
+	server = app.listen(8081, () => {
+	  console.log('Test runing at http://localhost:8081')
+	})
   })
-  after()
+  
+  it()
+  
+  after(done => server.close())
 })
