078717b370273c53f853b47d111a0e1e43f276dc
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-05-15 23:21:56 -0300

    connect added in used with middleware, but not works

--- src/main/config/node/app.ts
+++ src/main/config/node/app.ts
@@ -14,41 +14,4 @@ middlewares.use(handler)
 const app = createServer(middlewares)
   .listen(port, () => console.log('server is running'))
 
-// app.on('request', function (request, response) {
-//   console.log(`METHOD: ${request.method}; URL: ${request.url}`)
-//   switch (request.method) {
-//     case 'GET':
-//     case 'PUT':
-//     case 'POST':
-//     case 'PATCH':
-//     case 'DELETE':
-//       response.writeHead(200, {
-//         'Content-Type': 'application/json'
-//       })
-//       response.end(JSON.stringify({ dummy: 'dummy' }))
-//       break
-//     default:
-//       response.writeHead(405, {
-//         'Content-Type': 'application/json'
-//       })
-//       response.end(JSON.stringify({ error: `method ${request.method} not allowed` }))
-//       break
-//   }
-// })
-
 export default app
-
-// if (request.url !== '/') {
-// const query = request.url ? parse(request.url, true) : false
-
-//   // let query
-
-//   // if (request.url !== '/' && request.url) {
-//   //   query = request.url
-//   //   console.log(query)
-//   // }
-
-//   console.log(query)
-//   // response.end('ok')
-// return
-// }
