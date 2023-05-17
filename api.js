0bbb76c339d1112b6b313de1990a3e5b914fd9e4
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-05-16 21:13:40 -0300

    unnecessary files removed

--- api.js
+++
@@ -1,8 +0,0 @@
-(async () => {
-  const webservicesUrl = 'localhost:6000'
-  const fetchRes = await fetch(webservicesUrl)
-
-  console.log(fetchRes.ok)
-
-  if (!fetchRes.ok) throw new Error(`HTTP error! Status: ${fetchRes.status}`);
-})()
