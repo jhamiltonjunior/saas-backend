c38d35133d6e7fca75511b4964c4a4c700b24e58
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-05-17 23:10:47 -0300

    now I have access adapterRouteShowTasks, but not works

--- src/main/adapters/node/tasks/routeAdapterShowUnique.ts
+++ src/main/adapters/node/tasks/routeAdapterShowUnique.ts
@@ -5,22 +5,20 @@ import { IHttpRequest } from '../../../../adapters/http/controllers/ports/http'
 import { ShowUniqueTasksController } from '../../../../adapters/http/controllers/tasks/showUniqueTasksController'
 import { ShowAllTasksController } from '../../../../adapters/http/controllers/tasks/showAllTasksController'
 
-export const adapterRouteShowTasks = (
-  controller: ShowUniqueTasksController | ShowAllTasksController
-): any => {
-  return async (request: IncomingMessage, response: ServerResponse) => {
-    const query = request.url ? parse(request.url, true) : false
-
-    console.log(query)
-
-    const httpRequest: IHttpRequest = {
-      params: query
-    }
-
-    const httpResponse = await controller.handle(httpRequest)
-
-    response.writeHead(httpResponse.statusCode)
-    response.write(httpResponse.body)
-    response.end()
+export const adapterRouteShowTasks = async (
+  controller: ShowUniqueTasksController | ShowAllTasksController,
+  request: IncomingMessage, response: ServerResponse
+): Promise<any> => {
+  const query = request.url ? parse(request.url, true) : false
+  const httpRequest: IHttpRequest = {
+    params: query
   }
+
+  const httpResponse = await controller.handle(httpRequest)
+  console.log(httpResponse)
+  console.log('entrei')
+
+  // response.writeHead(httpResponse.statusCode)
+  // response.write(httpResponse.body)
+  response.end()
 }
--- src/main/routes/node/tasks.ts
+++ src/main/routes/node/tasks.ts
@@ -8,23 +8,33 @@ import { makeShowAllTasksController } from '../../factories/tasks/showAll'
 // import { makeDeleteTasksController } from '../../factories/tasks/delete'
 // import { makeUpdateTasksController } from '../../factories/tasks/update'
 
-export default (request: IncomingMessage, response: ServerResponse): void => {
+const handler = (request: IncomingMessage, response: ServerResponse) => {
+  response.writeHead(200)
+  response.end(`${response.statusCode}`)
+}
+
+export default async (request: IncomingMessage, response: ServerResponse): Promise<void> => {
   if (request.url === '/' && request.method === 'GET') {
-    response.writeHead(200)
-    response.end('ok')
+    return handler(request, response)
   }
 
   if (request.url === '/tasks' && request.method === 'GET') {
-    adapterRouteShowTasks(makeShowAllTasksController())
-    response.end('ok')
+    // response.end('ok')
+    await adapterRouteShowTasks(
+      makeShowAllTasksController(),
+      request, response
+    )
     return
   }
 
-  if (request.url === '/tasks/:url' && request.method === 'GET') {
-    adapterRouteShowTasks(makeShowUniqueTasksConstroller())
-    response.end('ok')
-    return
-  }
+  // if (request.url === '/tasks/:url' && request.method === 'GET') {
+  //   adapterRouteShowTasks(
+  //     makeShowUniqueTasksConstroller()
+  //     // request, response
+  //   )
+  //   response.end('ok')
+  //   return
+  // }
 
   if (response.statusCode === 404) {
     response.end('This Page does not exist!')
