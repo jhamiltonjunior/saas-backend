fcb6ba0f1f0df6709235de00b7489ca66cfae56b
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-06-06 00:24:47 -0300

    test: is possible test if the return of database is object

---
+++ test/integration/database/postgresTasksRepository.test.ts
@@ -0,0 +1,30 @@
+import { describe, after, before, it } from 'node:test'
+import { strictEqual, fail, ok } from 'node:assert'
+
+import { PostgresTasksRepository } from '../../../src/external/database/postgreSQL/tasks/postgresTasksRepository'
+
+export const connectionObject = {
+  user: 'postgres',
+  host: '172.17.0.3',
+  database: 'postgres',
+  password: '0000',
+  port: 5432,
+}
+
+describe('The integration with PostgreSQL database', () => {
+  it('Return should be a object', async () => {
+    const result = new PostgresTasksRepository(
+      connectionObject
+    ).findAllTasks()
+
+    strictEqual(typeof result, typeof Object())
+  })
+
+  it('Should be possible get informations', async () => {
+    const result = new PostgresTasksRepository(
+      connectionObject
+    ).findAllTasks()
+
+    strictEqual(typeof result, typeof Object())
+  })
+})
