481fee2478e3625078031f9a56ec463c0891821d
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-08-26 19:08:34 -0300

    Feat: I added a new route for check the token

--- src/main/routes/tasks.ts
+++ src/main/routes/tasks.ts
@@ -6,12 +6,15 @@ import { makeShowUniqueTasksConstroller } from '../factories/tasks/showUnique'
 import { makeShowAllTasksController } from '../factories/tasks/showAll'
 import { makeDeleteTasksController } from '../factories/tasks/delete'
 import { makeUpdateTasksController } from '../factories/tasks/update'
+import { auth } from '../middleware/express/authOnly'
 
 export default (router: Router): void => {
   router.get('/tasks', adapterRouteShowTasks(makeShowAllTasksController()))
   router.get('/tasks/find/:url', adapterRouteShowTasks(makeShowUniqueTasksConstroller()))
 
   router.post('/tasks/create', adapterRouteWithAuthentication(makeCreateTasksController()))
+
+  router.post('/tasks/verify', auth, (_, res) => res.status(200).json({ message: 'ok' }))
 
   router.put('/tasks/edit/:url', adapterRouteWithAuthentication(makeUpdateTasksController()))
 
